648dd6652ca25b055a2f0cbe64bfccc05634e911
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-07-28 19:35:45 +0300

    IDE script support: cancel coroutines on project disposal

--- idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/ScriptDependenciesUpdater.kt
+++ idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/ScriptDependenciesUpdater.kt
@@ -212,7 +212,7 @@ internal class ScriptDependenciesUpdater(
     }
 
     fun notifyRootsChanged() {
-        launch(EDT) {
+        launch(EDT + project) {
             runWriteAction {
                 ProjectRootManagerEx.getInstanceEx(project)?.makeRootsChange(EmptyRunnable.getInstance(), false, true)
                 ScriptDependenciesModificationTracker.getInstance(project).incModificationCount()
